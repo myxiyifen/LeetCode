d0f0b4d5b7889c95e5bffb38d7448a9ce3072a94
xiyifen <1017665216@qq.com>
2017-07-14 23:24:03 +0800

    测试

---
+++ src/TestDemo.java
@@ -0,0 +1,10 @@
+/**
+ * Created by User on 2017/7/14.
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        int a=1,b=2;
+        System.out.println(a+b);
+
+    }
+}
